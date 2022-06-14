9d8e20da28e91aa0655097a2b2e211ea4ba07578
SatyaDanthuluri <54783024+DanthuluriSatya@users.noreply.github.com>
2022-06-14 15:55:09 -0500

    Angular Code Challange
    Added displaying most two popular lobs on dash board
    Added quote counts of a given lob on lob details page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LineOfBusiness } from '../LineOfBusiness';
 import { LineOfBusinessService } from '../lineOfBusiness.service';
+import {InMemoryDataService} from '../in-memory-data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,15 +10,51 @@ import { LineOfBusinessService } from '../lineOfBusiness.service';
 })
 export class DashboardComponent implements OnInit {
   linesOfBusiness: LineOfBusiness[] = [];
+  popularLinesOfBusiness: LineOfBusiness[] = [];
 
-  constructor(private lineOfBusinessService: LineOfBusinessService) { }
+  constructor(private lineOfBusinessService: LineOfBusinessService, private inMemoryService: InMemoryDataService) { }
 
   ngOnInit() {
     this.getLinesOfBusiness();
+    this.getPopularLineOFBusiness();
   }
 
   getLinesOfBusiness(): void {
     this.lineOfBusinessService.getLinesOfBusiness()
       .subscribe(linesOfBusiness => this.linesOfBusiness = linesOfBusiness.slice(1, 4));
+  }
+
+  getPopularLineOFBusiness() {
+    const lobs = this.inMemoryService.createDb();
+    const self = this;
+    // @ts-ignore
+    const quoteCounts = lobs.recentQuotes.reduce((a, {lineOfBusiness}) => (a[lineOfBusiness] = (a[lineOfBusiness] || 0) + 1, a), {});
+    console.log("2222", self.popularLinesOfBusiness)
+    lobs.linesOfBusiness.forEach(function(lob){
+      if (self.popularLinesOfBusiness.length === 0){
+        // inserting directly into empty list in first iteration
+        self.popularLinesOfBusiness.push(lob);
+      }else if (self.popularLinesOfBusiness.length === 1){
+        // comparing 2nd iteration with existing element at index 0.
+        // @ts-ignore
+        if (quoteCounts[self.popularLinesOfBusiness[0].id] > quoteCounts[lob.id]){
+          self.popularLinesOfBusiness.push(lob);
+        }else {
+          self.popularLinesOfBusiness.splice(0,0, lob);
+        }
+      }else {
+        // comparing both existing elements from 3rd iteration
+        // @ts-ignore
+        if(quoteCounts[self.popularLinesOfBusiness[0].id] < quoteCounts[lob.id]){
+          self.popularLinesOfBusiness[1] = self.popularLinesOfBusiness[0];
+          self.popularLinesOfBusiness[0] = lob;
+        }else { // @ts-ignore
+          if (quoteCounts[self.popularLinesOfBusiness[1].id] < quoteCounts[lob.id]) {
+                    self.popularLinesOfBusiness[1] = lob;
+                  }
+        }
+      }
+    });
+    console.log("popular lobs", quoteCounts, self.popularLinesOfBusiness);
   }
 }
--- src/app/lineOfBusiness-detail/lineOfBusiness-detail.component.ts
+++ src/app/lineOfBusiness-detail/lineOfBusiness-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { LineOfBusiness } from '../LineOfBusiness';
 import { LineOfBusinessService } from '../lineOfBusiness.service';
+import {InMemoryDataService} from "../in-memory-data.service";
 
 @Component({
   selector: 'app-lineOfBusiness-detail',
@@ -12,11 +13,13 @@ import { LineOfBusinessService } from '../lineOfBusiness.service';
 })
 export class LineOfBusinessDetailComponent implements OnInit {
   lineOfBusiness: LineOfBusiness | undefined;
+  quoteCounts = 0 ;
 
   constructor(
     private route: ActivatedRoute,
     private lineOfBusinessService: LineOfBusinessService,
-    private location: Location
+    private location: Location,
+    private inMemoryService: InMemoryDataService
   ) {}
 
   ngOnInit(): void {
@@ -27,6 +30,7 @@ export class LineOfBusinessDetailComponent implements OnInit {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.lineOfBusinessService.getLineOfBusiness(id)
       .subscribe(lineOfBusiness => this.lineOfBusiness = lineOfBusiness);
+    this.getQuoteCount(id);
   }
 
   goBack(): void {
@@ -38,5 +42,13 @@ export class LineOfBusinessDetailComponent implements OnInit {
       this.lineOfBusinessService.updateLineOfBusiness(this.lineOfBusiness)
         .subscribe(() => this.goBack());
     }
+  }
+
+  getQuoteCount(id: number) {
+    const lobs = this.inMemoryService.createDb();
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.quoteCounts = lobs.recentQuotes.filter(quote => quote.lineOfBusiness === id).reduce((a, {lineOfBusiness}) => (a[lineOfBusiness] = (a[lineOfBusiness] || 0) + 1, a), {})[id];
+    console.log('123', this.quoteCounts);
   }
 }
